e3563a3b1273b81d60d7d145bdd430235d0ce09a
david offenbacher <davidoffenbacher@fullsail.edu>
2014-06-25 11:02:58 -0700

    Updated Project_3 to Project_4 / fixed errors

---
+++ Project_4/images/Facebook_files/YT3KHj2muA-.js
@@ -0,0 +1,5 @@
+/*1374691333,178183189*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["8eJ2b"]); }
+
+__d("legacy:PhotoSnowliftAds",["PhotoSnowliftAds"],function(a,b,c,d){a.PhotoSnowliftAds=b('PhotoSnowliftAds');},3);
